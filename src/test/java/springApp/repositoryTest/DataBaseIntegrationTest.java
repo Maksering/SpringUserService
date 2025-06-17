cc8e9ea053c85ace11242ad8376a5f09061e9bf2
Maksering <maksering.off@gmail.com>
2025-06-17 14:21:16 +0300

    moved DataBaseIntegrationTest

--- src/test/java/springApp/repositoryTest/DataBaseIntegrationTest.java
+++ src/test/java/springApp/repositoryTest/DataBaseIntegrationTest.java
@@ -1,8 +1,9 @@
-package springApp;
+package springApp.repositoryTest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import springApp.IntegrationTestInitClass;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
