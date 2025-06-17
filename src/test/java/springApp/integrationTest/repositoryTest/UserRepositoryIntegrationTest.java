3bc7c528df8cf62727201a254bd29316ddc2db9c
Maksering <maksering.off@gmail.com>
2025-06-17 12:05:48 +0300

    Rename RepositoryIntegrationTest to UserRepositoryIntegrationTest

--- src/test/java/springApp/integrationTest/repositoryTest/UserRepositoryIntegrationTest.java
+++ src/test/java/springApp/integrationTest/repositoryTest/UserRepositoryIntegrationTest.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class RepositoryIntegrationTest extends IntegrationTestInitClass {
+public class UserRepositoryIntegrationTest extends IntegrationTestInitClass {
 
     @Autowired
     UserRepository userRepository;
